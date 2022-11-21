35fc758370a27bbc2dccbdd576e01eac26acb6e8
iliya <MikuHutsune.Ru@yandex.ru>
2022-11-22 00:36:58 +0300

    Добавил алгоритм поиска

--- src/services/SearchManager.java
+++ src/services/SearchManager.java
@@ -19,13 +19,13 @@ public class SearchManager {
         return min;
     }
 
-    public static int levenshteinDistance(String s1, String s2) {
-        int[][] matrix = new int[s1.length() + 1][s2.length() + 1];
+    public static int levenshteinDistance(String desired, String received) {
+        int[][] matrix = new int[desired.length() + 1][received.length() + 1];
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
-                matrix[i][j] = levDiv(i, j, s1, s2, matrix);
+                matrix[i][j] = levDiv(i, j, desired, received, matrix);
             }
         }
-        return matrix[s1.length()][s2.length()];
+        return matrix[desired.length()][received.length()];
     }
 }
